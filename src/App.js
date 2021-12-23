8975675f19c02ae0ea02f6c42ba1e9e17838ded1
nazim725 <nazimhabib77@gmail.com>
2021-12-22 23:30:46 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import logo from './logo.svg';
+import './App.css';
+import ShowExpenses from './Components/ShowExpenses/ShowExpenses';
+import '../src/Components/ShowExpenses/ShowExpenses.css'
+
+const expenses = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { 
+    id: 'e2', 
+    title: 'New TV', 
+    amount: 799.49, 
+    date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
+function App() {
+  return (
+    <div className='expenses'>
+      <h2 className='heading'>Lets Get started!!!</h2>
+      {expenses.map(expense=> <ShowExpenses expense={expense}></ShowExpenses>)}
+     
+    </div>
+  );
+}
+
+export default App;
